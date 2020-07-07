2e17fb8c484665b946e7045f7b90d60dafe5f097
蛋黄 <lijia@pampas.com>
2020-07-07 10:04:06 +0800

    bugfix

--- leetcode/0086.partition.js
+++ leetcode/0086.partition.js
@@ -4,37 +4,29 @@
  * @return {ListNode}
  */
 var partition = function(head, x) {
-    let dummy = new ListNode(0)
-    let d = dummy
-    dummy.next  = head
-    let big =null
-    let small = new ListNode(0)
-    let currentSmall = small
-    while(d && d.next) {
-        if (d.next.val >= x) {
-            big = big ? big : d.next
-            d = d.next
+    let max = new ListNode(0)
+    let maxNode = max
+    let min = new ListNode(0)
+    let minNode = min
+    let node = head
+    while (node) {
+        if(node.val <x) {
+            minNode.next = node
+            minNode = minNode.next
         } else {
-            currentSmall.next =d.next
-            if(big) {
-                d.next = d.next.next
-                dummy.next = small.next
-                currentSmall.next.next = big
-            }
-            currentSmall =currentSmall.next
-            d =d.next
+            maxNode.next = node
+            maxNode = maxNode.next
         }
+        node = node.next
     }
-    return dummy.next
+    maxNode.next = null
+    minNode.next = max.next
+    return min.next
 };
 function ListNode(val) {
     this.val = val
     this.next = null
 }
-let  a = new ListNode(1)
-a.next = new ListNode(2)
-a.next.next = new ListNode(3)
-a.next.next.next = new ListNode(2)
-a.next.next.next.next = new ListNode(5)
-a.next.next.next.next.next = new ListNode(2)
+let  a = new ListNode(2)
+a.next = new ListNode(1)
 console.log(partition(a,2))
