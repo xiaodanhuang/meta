13730264d519709972524c546b17b9e2be3b57db
蛋黄 <lijia@pampas.com>
2020-07-08 14:03:44 +0800

    update

--- leetcode/0142.detectCycle.js
+++ leetcode/0142.detectCycle.js
@@ -3,14 +3,15 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    let fast = head
-    let slow = head
-    while(fast && fast.next) {
-        slow = slow.next
-        fast= fast.next.next
-        if(slow === fast) {
-            return slow
+    let node = head
+    let map = new Map()
+
+    while(node) {
+        if(map.get(node)) {
+            return node
         }
+        map.set(node,node)
+        node = node.next
     }
     return null
 };
