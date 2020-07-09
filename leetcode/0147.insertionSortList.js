860c0e3e92da1a98479e59422dee62f085d6a271
蛋黄 <lijia@pampas.com>
2020-07-09 14:43:33 +0800

    update

--- leetcode/0147.insertionSortList.js
+++ leetcode/0147.insertionSortList.js
@@ -2,34 +2,37 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var insertionSortList = function(head) {
+var insertionSortList = function (head) {
     let node = new ListNode(0)
     node.next = head
     let cur = new ListNode(0)
-    while ( node && node.next){
-        let beforeNode = findBeforeNode(cur,node.next)
-        beforeNode = beforeNode ? beforeNode :cur
-        let next =  beforeNode.next
+    while (node && node.next) {
+        let beforeNode = findBeforeNode(cur, node.next)
+        beforeNode = beforeNode ? beforeNode : cur
+        let next = beforeNode.next
         beforeNode.next = node.next
         node.next = node.next.next
         beforeNode.next.next = next
     }
     return cur.next
 };
-function findBeforeNode (cur,node) {
 
-   while (cur && cur.next) {
-       if(cur.next.val > node.val) {
-           return cur
-       }
-       cur = cur.next
-   }
+function findBeforeNode(cur, node) {
+
+    while (cur && cur.next) {
+        if (cur.next.val > node.val) {
+            return cur
+        }
+        cur = cur.next
+    }
     return cur
 }
+
 function ListNode(val) {
     this.val = val
     this.next = null
 }
+
 let a = new ListNode(4)
 a.next = new ListNode(2)
 a.next.next = new ListNode(1)
